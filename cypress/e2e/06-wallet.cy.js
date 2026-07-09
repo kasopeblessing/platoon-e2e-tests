d63533eb715f3ded79ad328a27c6955615e10e8a
Kasope Adebowale <kasopeblessing@gmail.com>
2026-07-09 10:33:42 +0100

    Update Wallet Flow

--- cypress/e2e/06-wallet.cy.js
+++ cypress/e2e/06-wallet.cy.js
@@ -12,14 +12,14 @@ beforeEach(() => {
   })
 
   it('Manage Wallet button goes to wallet page', () => {
-    cy.contains('Manage Wallet').click({force: true})
+    cy.contains('Manage Wallet', {timeout: 10000}).click({force: true})
     cy.url().should('include', '/mywallet/accounts')
     cy.contains('Wallet Balance').should('be.visible')
     cy.contains('Available balance').should('be.visible')
   })
 
   it('Transaction History loads properly', () => {
-    cy.contains('Manage Wallet').click({force: true})
+    cy.contains('Manage Wallet', {timeout: 10000}).click({force: true})
     cy.url().should('include', '/mywallet/accounts')
     cy.contains('Transaction History').click({force: true})
     cy.url().should('include', '/mywallet/transactions')
